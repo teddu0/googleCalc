f0897fdf1b723b672aa038cfffe3dc7a242b0ba2
teddu0 <107142125+teddu0@users.noreply.github.com>
2022-07-29 15:56:16 +0300

    Добавил проверку на наличие окна для принятия cookie

--- src/test/java/com/google/calculator/GooglePage.java
+++ src/test/java/com/google/calculator/GooglePage.java
@@ -1,19 +1,22 @@
 package com.google.calculator;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.testng.Assert;
 
 import static com.google.calculator.BaseSeleniumPage.driver;
+import static org.openqa.selenium.Keys.ENTER;
 
 
 public class GooglePage {
     private static final String RESULT = "0";
 
     public void fillSearchField(String calculator) {
+        if (isElementPresent(By.xpath("//button[@id='L2AGLb']"))) {
+            driver.findElement(By.xpath("//button[@id='L2AGLb']")).click();
+        }
         driver.findElement(By.xpath("//input[@type='text']")).sendKeys(calculator);
-        driver.findElement(By.xpath("//center//input[@type='submit'][1]")).click();
+        driver.findElement(By.xpath("//input[@type='text']")).sendKeys(ENTER);
+       // driver.findElement(By.xpath("//center//input[@type='submit']")).sendKeys(ENTER);
     }
 
     public void typeCalcButtons() {
@@ -30,5 +33,14 @@ public class GooglePage {
     public void getResult() {
         WebElement result = driver.findElement(By.xpath("//span[@id='cwos']"));
         Assert.assertEquals(result.getText(), RESULT);
+    }
+
+    public boolean isElementPresent (By locator) {
+        try{
+            driver.findElement(locator);
+            return true;
+        }catch (NoSuchElementException ex) {
+            return false;
+        }
     }
 }
